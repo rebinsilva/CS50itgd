a22d3afe6819431882a12d0aa1e55ac0978a6e16
Rebin Silva <rebinsilva@gmail.com>
2022-06-01 15:24:52 +0530

    Assignment 4 done

---
+++ assignment4/src/GameObject.lua
@@ -0,0 +1,48 @@
+--[[
+    GD50
+    -- Super Mario Bros. Remake --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+GameObject = Class{}
+
+function GameObject:init(def)
+    self.x = def.x
+    self.y = def.y
+    self.texture = def.texture
+    self.width = def.width
+    self.height = def.height
+    self.frame = def.frame
+    self.solid = def.solid
+    self.collidable = def.collidable
+    self.consumable = def.consumable
+    self.onCollide = def.onCollide
+    self.onConsume = def.onConsume
+    self.hit = def.hit
+    self.animation = def.animation
+    self.hide = def.hide
+
+    if not self.animation then
+	self.animation = Animation {frames = {self.frame}}
+    end
+end
+
+function GameObject:collides(target)
+    return not (target.x > self.x + self.width or self.x > target.x + target.width or
+            target.y > self.y + self.height or self.y > target.y + target.height)
+end
+
+function GameObject:update(dt)
+    if self.animation then
+	self.animation:update(dt)
+    end
+
+end
+
+function GameObject:render()
+    if not self.hide then
+	love.graphics.draw(gTextures[self.texture], gFrames[self.texture][self.animation:getCurrentFrame()], self.x, self.y)
+    end
+end
